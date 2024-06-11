6e7d7c04ff03ed6bd5e52332e269f5a115de46f1
aiden-gos <aiden.phan.gos@gmail.com>
2024-06-11 15:18:16 +0700

    Fix ratingbar

--- resources/views/rating.blade.php
+++ resources/views/rating.blade.php
@@ -12,30 +12,35 @@
                     <textarea class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
                         name="content" id="content" cols="50" rows="4"></textarea>
                     <div class="flex flex-col justify-around">
-                        <input type="number" name="star" />
+
+                        @include('components.ratingbar')
+
                         <button class="bg-black text-white w-full py-3 rounded-md max-w-64">Send</button>
                     </div>
                 </form>
             </div>
         @endif
 
-        @if($rate[0] && $rate[0]->author->id == Auth::user()->id)
-        {{-- form update hidden --}}
-        <div id='update-form-rate' class="flex flex-row gap-5 hidden">
-            <div>Comment</div>
-            <form method="post" action="{{ route('rate.update', $rate[0]->id) }}" class="flex flex-row gap-5 w-full">
-                @csrf
-                @method('patch')
-                <input type="hidden" name="user_id" value="{{ $user->id }}">
-                <textarea class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
-                    name="content" id="content" cols="50" rows="4" id='text-update'>{{$rate[0]->content}}</textarea>
-                <div class="flex flex-col justify-around">
-                    <input type="number" name="star" id='star-update' value="{{$rate[0]->star}}"/>
-                    <button class="bg-black text-white w-full py-3 rounded-md max-w-64">Update</button>
-                </div>
-            </form>
-        </div>
-        {{--  --}}
+        @if ($rate[0] && $rate[0]->author->id == Auth::user()->id)
+            {{-- form update hidden --}}
+            <div id='update-form-rate' class="flex flex-row gap-5 hidden">
+                <div>Comment</div>
+                <form method="post" action="{{ route('rate.update', $rate[0]->id) }}"
+                    class="flex flex-row gap-5 w-full">
+                    @csrf
+                    @method('patch')
+                    <input type="hidden" name="user_id" value="{{ $user->id }}">
+                    <textarea class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+                        name="content" id="content" cols="50" rows="4" id='text-update'>{{ $rate[0]->content }}</textarea>
+                    <div class="flex flex-col justify-around">
+
+                        @include('components.ratingbar')
+
+                        <button class="bg-black text-white w-full py-3 rounded-md max-w-64">Update</button>
+                    </div>
+                </form>
+            </div>
+            {{--  --}}
         @endif
 
         @forelse($rate as $item)
@@ -50,6 +55,4 @@
         </div>
     </div>
 </div>
-<script>
-
-</script>
+<script></script>
