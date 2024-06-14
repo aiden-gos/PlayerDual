ff2b294b80594f02c0bf9570c6f0fdf0457ef40b
aiden-gos <aiden.phan.gos@gmail.com>
2024-06-14 11:08:43 +0700

    Fix increa view

--- routes/stories.php
+++ routes/stories.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\StoriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/stories', [StoriesController::class, 'index'])->name('stories');
+Route::get('/stories/view/{id}', [StoriesController::class, 'updateView'])->name('stories.view');
 Route::middleware('auth')->group(function () {
     Route::post('/stories', [StoriesController::class, 'store'])->name('stories.up');
-    Route::get('/stories/view/{id}', [StoriesController::class, 'updateView'])->name('stories.view');
     Route::get('/stories/like/{id}', [StoriesController::class, 'updateLike'])->name('stories.like');
     Route::get('/stories/unlike/{id}', [StoriesController::class, 'updateUnLike'])->name('stories.unlike');
 });
