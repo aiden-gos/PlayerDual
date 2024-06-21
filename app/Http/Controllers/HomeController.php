0c389e113f4ed71989098e71217e86570ca0e8eb
aiden-gos <aiden.phan.gos@gmail.com>
2024-06-21 14:24:53 +0700

    Add service

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,71 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Game;
-use App\Models\Story;
-use App\Models\User;
+use App\Services\HomeService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
+    protected $homeService;
+
+    public function __construct()
+    {
+        $this->homeService = new HomeService();
+    }
+
     public function index(Request $request)
     {
-        $vip_user = User::query()->orderBy("balance", "DESC")->limit(15)->get();
-        $hot_user = User::query()->orderBy("price", "DESC")->limit(15)->get();
-        $games = Game::all();
-        $stories = Story::where('status', 'open')->orderBy('created_at', 'desc')->take(10)->get();
-        return view('home', [
-            'vip_user' => $vip_user,
-            'hot_user' => $hot_user,
-            'games' => $games,
-            'stories' => $stories
-        ]);
+        return $this->homeService->index();
     }
 
     public function search(Request $request)
     {
-        $user = User::query();
-
-        if (!empty($request->query("name"))) {
-
-            $user->where('name', 'like', '%' . $request->query('name') . '%');
-        }
-        if ($request->query("sex") == "1" || $request->query("sex") == "0") {
-            $user->where("sex", $request->query("sex"));
-        }
-        if (!empty($request->query("priceMin"))) {
-            $user->where("price", ">", $request->query("priceMin"));
-        }
-        if (!empty($request->query("priceMax"))) {
-            $user->where("price", "<", $request->query("priceMax"));
-        }
-        if (!empty($request->query("priceMax"))) {
-            $user->where("price", "<", $request->query("priceMax"));
-        }
-        if (!empty($request->query("game"))) {
-            $gameId = $request->query("game");
-            $user->whereHas('games', function ($query) use ($gameId) {
-                $query->where('game_id', $gameId);
-            });
-        }
-
-        $reslut = $user->limit(20)->get();
-        return response()->json($reslut, 200);
+        return $this->homeService->search($request);
     }
 
     public function filterGame(Request $request)
     {
-        $user = User::query();
-
-        if (!empty($request->route("game"))) {
-            $gameId = $request->route("game");
-            $user->whereHas('games', function ($query) use ($gameId) {
-                $query->where('game_id', $gameId);
-            });
-        }
-
-        $reslut = $user->paginate(20);
-        return response()->json($reslut, 200);
+        return $this->homeService->filterGame($request);
     }
 }
